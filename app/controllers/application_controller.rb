857080bf198cf0c5a9afb6ba05300459fc7fabf0
Shotaro Hirose <shotaro199959@icloud.com>
2020-09-02 00:20:21 +0900

    [Add] イベントの登録機能を作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < ActionController::Base
-  helper_method :logged_in? # コントローラとビューの両方から利用するのでhelper_methodで宣言
+  before_action :authenticate
+  helper_method :logged_in?, :current_user # コントローラとビューの両方から利用するのでhelper_methodで宣言
 
   private
 
   def logged_in?
-    !!session[:user_id] # not演算子を２つ重ねることで、session[:user_id]がfalseまたはnilの時はfalseを、それ以外はtrueへ変換
+    !!current_user
+  end
+
+  def current_user
+    return unless session[:user_id]
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def authenticate
+    return if logged_in?
+    redirect_to root_path, alert: "ログインしてください"
   end
 end
