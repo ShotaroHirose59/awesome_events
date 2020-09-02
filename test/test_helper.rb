87ee4b7cb7067ca9c546d1ab90f38cfe178dd118
Shotaro Hirose <shotaro199959@icloud.com>
2020-09-03 04:34:00 +0900

    [Add] システムテスト

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
+require_relative 'sign_in_helper'
 require 'rails/test_help'
-require 'factory_bot_rails'
-include FactoryBot::Syntax::Methods
+
 
 # Pry.hooks.add_hook(:before_session, "use FactoryBot shorthand") do |output, binding, pry|
   # Object.include FactoryBot::Syntax::Methods
@@ -11,7 +11,6 @@ include FactoryBot::Syntax::Methods
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
-  #config.include FactoryBot::Syntax::Methods
-  #include FactoryBot::Syntax::Methods
+
   # Add more helper methods to be used by all tests here...
 end
